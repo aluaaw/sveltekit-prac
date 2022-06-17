79c535cf8b6e40ac065eb280dfd2f0c731723485
aluaaw <ylua.aw@gmail.com>
2022-06-17 14:42:25 +0900

    init prettier, eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,32 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	extends: [
+		'airbnb-typescript',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
+	ignorePatterns: [
+		'playwright.config.ts',
+		'env.d.ts',
+		'*.cjs',
+		'tests/*.ts',
+		'*.js',
+	],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte3/typescript': () => require('typescript'),
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: ['./tsconfig.json'],
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
+		node: true,
+	},
 };
